979595e8052b86198772390ce12ae44dba9c893c
hugh140 <hugofer300@gmail.com>
2023-04-08 16:33:05 -0500

    Creating folders functionality finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import Folder from "./components/Folder";
 import "./index.css";
 
 function App() {
+  const [directory, setDirectory] = useState([]);
+  // console.log(directory)
+
+  function newFolderClick() {
+    const newFolderName = prompt('Ingresar el nombre de la carpeta a crear.')
+    if (newFolderName === null) return;
+
+    const newDirectory = [...directory];
+    newDirectory.push({ name: newFolderName });
+    setDirectory(newDirectory);
+  }
+
   return (
     <>
-      <Folder folderName="Hola" />
+      <button
+        className="rounded-lg bg-green-100 p-3 text-green-600 hover:bg-green-200"
+        onClick={newFolderClick}
+      >
+        Nueva Carpeta
+      </button>
+
+      {directory.map((folder, index) => (
+        <Folder key={index} folderName={folder.name}></Folder>
+      ))}
+
+      {/* <Folder folderName="Hola" />
       <div className="border-l-2">
-        <div className="ms-auto w-11/12">
+        <div className="ms-auto w-[95%]">
           <Folder />
           <Folder />
           <Folder />
           <div className="border-l-2">
-            <div className="ms-auto w-11/12">
+            <div className="ms-auto w-[95%]">
               <Folder />
               <Folder />
               <Folder />
@@ -20,7 +44,7 @@ function App() {
         </div>
       </div>
       <Folder />
-      <Folder />
+      <Folder /> */}
     </>
   );
 }
--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,24 +1,24 @@
-function Folder({folderName = 'Foo Text'}) {
+function Folder({ folderName = "Foo Text" }) {
   return (
     <div
-      className="relative mx-auto mt-5 rounded-xl border-2 border-zinc-400
-      p-3 text-xl text-zinc-800"
+      className="relative mx-auto mt-1 rounded-xl border-2
+      border-zinc-200 p-2 text-zinc-800"
     >
       {folderName}
       <div className="absolute right-3 top-1/2 -translate-y-1/2">
         <div className="grid grid-cols-2 gap-2">
-          <div
-            className="unded-lg top-1/2 w-10 cursor-pointer rounded-lg
-          bg-blue-100 p-1 text-center text-blue-500 "
+          <button
+            className="unded-lg top-1/2 w-10 rounded-lg bg-blue-100
+          p-1 text-center text-blue-500 hover:bg-blue-200 "
           >
             +
-          </div>
-          <div
-            className="unded-lg top-1/2 w-10 cursor-pointer rounded-lg
-          bg-red-100 p-1 text-center text-red-500"
+          </button>
+          <button
+            className="unded-lg top-1/2 w-10 rounded-lg bg-red-100
+          p-1 text-center text-red-500 hover:bg-red-200"
           >
             -
-          </div>
+          </button>
         </div>
       </div>
     </div>
