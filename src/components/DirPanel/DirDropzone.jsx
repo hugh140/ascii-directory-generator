cebf591e71d3d42c3c7cbf93c9c48cd65ef450a0
hugh140 <hugofer300@gmail.com>
2023-04-14 14:04:55 -0500

    Saving changes for desktop

--- src/components/DirPanel/DirDropzone.jsx
+++ src/components/DirPanel/DirDropzone.jsx
@@ -5,13 +5,16 @@ import { useDropzone } from "react-dropzone";
 function DirDropzone() {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     multiple: false,
+    accept: {
+      "application/zip": [".zip"],
+    },
   });
 
   const files = acceptedFiles.map((file) => console.log(file));
   return (
     <div
-      {...getRootProps({ className: "dropzone" })}
-      className="mb-5 grid w-full cursor-pointer place-items-center border-2 border-gray-200 
+      {...getRootProps()}
+      className="mb-5 grid w-full cursor-pointer place-items-center border-2 border-dashed border-gray-200 
         p-7 text-center text-gray-500 focus-within:border-blue-400 lg:mb-0 lg:ms-5 lg:w-1/4"
     >
       <div>
