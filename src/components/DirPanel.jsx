c1d5f888aaf91a48ebe97d92f70e13c2e0fa691e
Hugo <hugofer300@gmail.com>
2023-04-13 23:44:55 -0500

    Add drag and drop panel

--- src/components/DirPanel.jsx
+++ src/components/DirPanel.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Directory from "./Directory";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 import getFolderIndex from "../scripts/getFolderIndex";
 
 function DirPanel({ root, folders }) {
@@ -50,23 +53,37 @@ function DirPanel({ root, folders }) {
         Nueva Carpeta
       </button>
 
-      <div className="h-[70vh] overflow-y-scroll border-2 border-x-gray-50 border-y-gray-200 p-2">
-        {!folders.length ? (
-          <div className="grid h-full place-items-center text-gray-500">
-            <div>
-              Agrega una nueva carpeta. <br />
-            </div>
+      <div className="lg:flex lg:flex-row-reverse">
+        <div
+          className="mb-5 grid w-full place-items-center border-2 
+        border-gray-200 p-7 text-center text-gray-500 focus-within:border-blue-400 lg:mb-0 lg:ms-5 lg:w-1/4"
+        >
+          {/* <input /> */}
+          <div>
+            <FontAwesomeIcon className="text-2xl" icon={faPlus} />
+            <br />
+            Arrastra archivos comprimidos
           </div>
-        ) : (
-          <Directory
-            newFolder={handleNewFolder}
-            editFolder={handleEditFolder}
-            removeFolder={handleRemoveFolder}
-            dirTree={directory.root.folders}
-          />
-        )}
+        </div>
+
+        <div className="h-[70vh] w-full overflow-y-scroll border-2 border-x-gray-50 border-y-gray-200 p-2">
+          {!folders.length ? (
+            <div className="grid h-full w-full place-items-center text-gray-500">
+              <div>
+                Agrega una nueva carpeta. <br />
+              </div>
+            </div>
+          ) : (
+            <Directory
+              newFolder={handleNewFolder}
+              editFolder={handleEditFolder}
+              removeFolder={handleRemoveFolder}
+              dirTree={directory.root.folders}
+            />
+          )}
+        </div>
       </div>
     </>
   );
 }
-export default DirPanel
+export default DirPanel;
